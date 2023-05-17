5f1b910d590cf3bf33a0c627f1fdc813d9b3305b
zhangyou <zhangyou@etongeis.com>
2023-05-17 19:04:25 +0800

    ttt

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,11 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component:      Home
+    component: Home
   },
   {
     path: '/about',
-    name: 'About',
+    name:      'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
